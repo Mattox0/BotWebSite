b08cd3fbf1e068eead3e160b233a139c6cb85354
MFERREIRASILVA <matteo.ferreirasilva@ynov.com>
2022-04-22 22:51:18 +0200

    add hidden

--- BotWebSite/src/Controller/CommandsController.php
+++ BotWebSite/src/Controller/CommandsController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class CommandsController extends AbstractController{
+class CommandsController extends AbstractController {
     
     #[Route('/commands', name: 'app_commands')]
     public function index(CommandRepository $commands) : Response {
